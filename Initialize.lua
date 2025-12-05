aea67c6dacb07a68ec7f193013729107a2eb4e38
j <122655895+jaeelin@users.noreply.github.com>
2025-12-05 01:10:25 -0500

    Create Initialize.lua

---
+++ Initialize.lua
@@ -0,0 +1,19 @@
+local Library = {}
+
+Library.Core = {
+	Window = require(script.Core.Window),
+}
+
+--[[Library.Components = {
+	Toggle = require(script.Components.Toggle),
+}
+
+Library.Theme = {
+	Dark = require(script.Theme.Dark),
+}
+
+Library.Utilities = {
+	Tween = require(script.Utils.Tween),
+}]]
+
+return Library
