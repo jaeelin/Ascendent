de2a8feccb2e9355c65a4b995f24267d8afc8027
j <122655895+jaeelin@users.noreply.github.com>
2025-12-05 18:49:21 -0500

    Update Window.lua

--- Core/Window.lua
+++ Core/Window.lua
@@ -1,28 +1,31 @@
 local coreGui = game:GetService("CoreGui")
 
-local Window = {}
-Window.__index = Window
+local protectGui = (syn and syn.protect_gui)
 
-function Window.new(config)
-	local self = setmetatable({}, Window)
-
+return function(config)
 	self.Title = config.Title or "Ascendent"
-	
-	local screenGui = Instance.new("ScreenGui")
-	screenGui.Name = self.Title
+
+	local screenGui = Instance.new("ScreenGui", coreGui)
+	screenGui.Name = config.Title
 	screenGui.ResetOnSpawn = false
 	screenGui.IgnoreGuiInset = true
-	screenGui.Parent = coreGui
 
-	local frame = Instance.new("Frame")
-	frame.Name = "Background"
-	frame.Size = UDim2.new(1, 0, 1, 0)
-	frame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-	frame.BackgroundTransparency = 1
-	frame.Parent = screenGui
-	self.Frame = frame
+	if protectGui then protectGui(screenGui) end
 
-	return self
+	local background = Instance.new("Frame", screenGui)
+	background.Name = "Background"
+	background.Size = UDim2.new(1, 0, 1, 0)
+	background.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+	background.BackgroundTransparency = 1
+
+	local mainFrame = Instance.new("Frame", background)
+	mainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+	mainFrame.BackgroundTransparency = 0.05
+	mainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+	mainFrame.Size = UDim2.new(0.35, 0, 0.5, 0)
+	mainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+	mainFrame.Parent = background
+
+	local uiCorner = Instance.new("UICorner", mainFrame)
+	uiCorner.CornerRadius = UDim.new(0.01, 0)
 end
-
-return Window
