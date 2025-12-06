85781d2051b6b47f62eabcc28739fdc48b302e84
j <122655895+jaeelin@users.noreply.github.com>
2025-12-05 20:36:02 -0500

    Update Initialize.lua

--- Initialize.lua
+++ Initialize.lua
@@ -4,8 +4,8 @@ Library.Core = {
 	Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/jaeelin/Ascendent/main/Core/Window.lua"))()
 }
 
-function Library:CreateWindow()
-	local Window = Library.Core.Window()
+function Library:CreateWindow(config)
+	local Window = Library.Core.Window(config)
 
 	return Window
 end
