db973d065c9ab17975c1e59101950c67e62e2d7c
j <122655895+jaeelin@users.noreply.github.com>
2025-12-06 14:41:44 -0500

    Create Tabs.lua

---
+++ Components/Tabs.lua
@@ -0,0 +1,24 @@
+local Tabs = {}
+
+function Tabs:Create(window, config)
+	local tabName = config.tabName or "NewTab"
+	local tabImage = config.tabImage or "rbxassetid://0"
+	
+	local tabButton = Instance.new("ImageButton", window.listFrame)
+	tabButton.Size = UDim2.new(1, 0, 0.15, 0)
+	tabButton.BackgroundTransparency = 1
+	tabButton.AutoButtonColor = false
+	tabButton.Name = tabName
+
+	local icon = Instance.new("ImageLabel", tabButton)
+	icon.BackgroundTransparency = 1
+	icon.Size = UDim2.new(0.7, 0, 0.7, 0)
+	icon.Position = UDim2.new(0.15, 0, 0.15, 0)
+	icon.Image = tabImage
+	
+	window.Tabs[tabName] = { Button = tabButton }
+
+	return window.Tabs[tabName]
+end
+
+return Tabs
