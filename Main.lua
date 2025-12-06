b9c0516de4d914be71a4b05834a0bf083281930a
j <122655895+jaeelin@users.noreply.github.com>
2025-12-06 01:23:29 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,17 @@
-local Library = {}
+local Library = {
+    Version = "1.0.0",
+    Window = nil
+}
 
 Library.Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/jaeelin/Ascendent/refs/heads/main/Core/Window.lua"))()
+
+function Library:CreateWindow(config)
+    if Library.Window then warn("multiple windows not supported") return end
+    
+    local Window = Library.Window(config)
+    Library.Window = Window
+
+    return Window
+end
 
 return Library
