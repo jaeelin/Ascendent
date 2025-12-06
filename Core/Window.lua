bda24826cc254d91f602006b5e2a2c3a1bca6b31
j <122655895+jaeelin@users.noreply.github.com>
2025-12-06 03:09:42 -0500

    Update Window.lua

--- Core/Window.lua
+++ Core/Window.lua
@@ -4,78 +4,103 @@ local userInputService = game:GetService("UserInputService")
 local protectGui = (syn and syn.protect_gui)
 
 return function(config)
-		local window = {}
+	local window = {}
 	
-		local title = config.Title or "Ascendent"
-		local keybind = config.Keybind
+	window.Tabs = {}
 	
-		window.MainGUI = Instance.new("ScreenGui", coreGui)
-		window.MainGUI.Name = title
-		window.MainGUI.ResetOnSpawn = false
-		window.MainGUI.IgnoreGuiInset = true
+	local title = config.Title or "Ascendent"
+	local keybind = config.Keybind
 
-		config.GUI = window.MainGUI
-	
-		if protectGui then protectGui(window.MainGUI) end
-	
-		window.background = Instance.new("Frame", window.MainGUI)
-		window.background.Name = "Background"
-		window.background.Size = UDim2.new(1, 0, 1, 0)
-		window.background.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-		window.background.BackgroundTransparency = 1
-	
-		window.mainFrame = Instance.new("Frame", window.background)
-		window.mainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-		window.mainFrame.BackgroundTransparency = 0.05
-		window.mainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-		window.mainFrame.Size = UDim2.new(0.35, 0, 0.5, 0)
-		window.mainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
-		window.mainFrame.Visible = false
-	
-		local uiCorner = Instance.new("UICorner", window.mainFrame)
-		uiCorner.CornerRadius = UDim.new(0.01, 0)
-		
-		window.sideBar = Instance.new("Frame", window.background)
-		window.sideBar.AnchorPoint = Vector2.new(0.5, 0.5)
-		window.sideBar.BackgroundTransparency = 1
-		window.sideBar.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-		window.sideBar.Size = UDim2.new(0.05, 0, 0.5, 0)
-		window.sideBar.Position = UDim2.new(-0.2, 0, 0.5, 0)
-		
-		local uiCorner2 = Instance.new("UICorner", window.sideBar)
-		uiCorner2.CornerRadius = UDim.new(0.025, 0)
-		
-		window.bar = Instance.new("Frame", window.sideBar)
-		window.bar.AnchorPoint = Vector2.new(0.5, 0.5)
-		window.bar.BackgroundTransparency = 0.05
-		window.bar.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-		window.bar.Size = UDim2.new(0.05, 0, 0.5, 0)
-		window.bar.Position = UDim2.new(1.2, 0, 0.5, 0)
-		
-		local uiCorner3 = Instance.new("UICorner", window.bar)
-		uiCorner3.CornerRadius = UDim.new(1, 0)
-		
-		window.listFrame = Instance.new("Frame", window.sideBar)
-		window.listFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-		window.listFrame.BackgroundTransparency = 1
-		window.listFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-		window.listFrame.Size = UDim2.new(1, 0, 1, 0)
-		window.listFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
-		
-		local uiCorner4 = Instance.new("UICorner", window.listFrame)
-		uiCorner4.CornerRadius = UDim.new(0.025, 0, 0)
-		
-		local uiListLayout = Instance.new("UIListLayout", window.listFrame)
-		uiListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
-		uiListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+	window.MainGUI = Instance.new("ScreenGui", coreGui)
+	window.MainGUI.Name = title
+	window.MainGUI.ResetOnSpawn = false
+	window.MainGUI.IgnoreGuiInset = true
 
-		userInputService.InputBegan:Connect(function(input)
-			if input.UserInputType == Enum.UserInputType.Keyboard then
-				if input.KeyCode == keybind then
-					window.MainGUI.Enabled = not window.MainGUI.Enabled
-				end
+	config.GUI = window.MainGUI
+
+	if protectGui then protectGui(window.MainGUI) end
+
+	window.background = Instance.new("Frame", window.MainGUI)
+	window.background.Name = "Background"
+	window.background.Size = UDim2.new(1, 0, 1, 0)
+	window.background.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+	window.background.BackgroundTransparency = 1
+
+	window.mainFrame = Instance.new("Frame", window.background)
+	window.mainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+	window.mainFrame.BackgroundTransparency = 0.05
+	window.mainFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+	window.mainFrame.Size = UDim2.new(0.35, 0, 0.5, 0)
+	window.mainFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+	window.mainFrame.Visible = false
+
+	local uiCorner = Instance.new("UICorner", window.mainFrame)
+	uiCorner.CornerRadius = UDim.new(0.01, 0)
+
+	window.sideBar = Instance.new("Frame", window.background)
+	window.sideBar.AnchorPoint = Vector2.new(0.5, 0.5)
+	window.sideBar.BackgroundTransparency = 1
+	window.sideBar.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+	window.sideBar.Size = UDim2.new(0.05, 0, 0.5, 0)
+	window.sideBar.Position = UDim2.new(-0.2, 0, 0.5, 0)
+
+	local uiCorner2 = Instance.new("UICorner", window.sideBar)
+	uiCorner2.CornerRadius = UDim.new(0.025, 0)
+
+	window.bar = Instance.new("Frame", window.sideBar)
+	window.bar.AnchorPoint = Vector2.new(0.5, 0.5)
+	window.bar.BackgroundTransparency = 0.05
+	window.bar.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+	window.bar.Size = UDim2.new(0.05, 0, 0.5, 0)
+	window.bar.Position = UDim2.new(1.2, 0, 0.5, 0)
+
+	local uiCorner3 = Instance.new("UICorner", window.bar)
+	uiCorner3.CornerRadius = UDim.new(1, 0)
+
+	window.listFrame = Instance.new("Frame", window.sideBar)
+	window.listFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+	window.listFrame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
+	window.listFrame.Size = UDim2.new(1, 0, 1, 0)
+	window.listFrame.Position = UDim2.new(0.5, 0, 0.5, 0)
+
+	local uiCorner4 = Instance.new("UICorner", window.listFrame)
+	uiCorner4.CornerRadius = UDim.new(0.25, 0, 0)
+
+	local uiStroke = Instance.new("UIStroke", window.listFrame)
+	uiStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+	uiStroke.Color = Color3.fromRGB(255, 255, 255)
+	uiStroke.Thickness = 1.5
+
+	local uiListLayout = Instance.new("UIListLayout", window.listFrame)
+	uiListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
+	uiListLayout.VerticalAlignment = Enum.VerticalAlignment.Center
+
+	userInputService.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.Keyboard then
+			if input.KeyCode == keybind then
+				window.MainGUI.Enabled = not window.MainGUI.Enabled
 			end
-		end)
-	
-		return window
+		end
+	end)
+
+	function window:CreateTab(tabName)
+		tabName = tabName or "NewTab"
+
+		local tabButton = Instance.new("ImageButton", window.listFrame)
+		tabButton.Size = UDim2.new(0.7, 0, 0.15, 0)
+		tabButton.BackgroundTransparency = 1
+		tabButton.AutoButtonColor = false
+		tabButton.Name = tabName
+
+		local imageIcon = Instance.new("ImageLabel", tabButton)
+		imageIcon.BackgroundTransparency = 1
+		imageIcon.Position = UDim2.new(0.1, 0, 0.1, 0)
+		imageIcon.Size = UDim2.new(0.78, 0, 0.78, 0)
+		
+		window.Tabs[tabName] = { Button = tabButton }
+		
+		return window.Tabs[tabName]
 	end
+
+	return window
+end
