e4724e30c6d24f53ed5cb9eeee173dfde5235882
j <122655895+jaeelin@users.noreply.github.com>
2025-12-07 21:51:21 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,39 +1,43 @@
 local Library = {
-    Version = "1.0.0",
-    Window = nil,
-    GUI = nil,
+	Version = "1.0.0",
+	Window = nil,
+	GUI = nil,
 }
 
 Library.Core = {
-    Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/jaeelin/Ascendent/refs/heads/main/Core/Window.lua"))()
+	Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/jaeelin/Ascendent/refs/heads/main/Core/Window.lua"))()
 }
 
 function Library:CreateWindow(config)
-    if Library.Window then warn("multiple windows not supported") return end
-    
-    local Window = Library.Core.Window({
+	if Library.Window then warn("multiple windows not supported") return end
+
+	local Window = Library.Core.Window({
 		Title = config.Title or "Ascendent",
 		Keybind = config.Keybind or Enum.KeyCode.LeftShift
 	})
-    Library.Window = Window
+	Library.Window = Window
 	Library.GUI = Window.MainGUI
+	
+	Window.exitButton.MouseButton1Click:Connect(function()
+		Library:Destroy()
+	end)
 
-    return Window
+	return Window
 end
 
 function Library:Destroy()
-    if Library.Window then
-        if Library.Window.MainGUI then
-            Library.Window.MainGUI:Destroy()
-        end
-		
-        Library.Window = nil
-        Library.GUI = nil
-    end
+	if Library.Window then
+		if Library.Window.MainGUI then
+			Library.Window.MainGUI:Destroy()
+		end
+
+		Library.Window = nil
+		Library.GUI = nil
+	end
 end
 
 function Library:GetVersion()
-    return Library.Version
+	return Library.Version
 end
 
 return Library
