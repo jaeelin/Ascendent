d817ea43d403c5f77f51e7a797fbe5614382ed3b
j <122655895+jaeelin@users.noreply.github.com>
2025-12-07 22:17:39 -0500

    Update Tabs.lua

--- Components/Tabs.lua
+++ Components/Tabs.lua
@@ -21,7 +21,7 @@ function Tabs:Create(window, config)
 	local icon = Instance.new("ImageLabel", tabButton)
 	icon.ScaleType = Enum.ScaleType.Fit
 	icon.BackgroundTransparency = 1
-	icon.Size = UDim2.new(0.9, 0, 0.7, 0)
+	icon.Size = UDim2.new(0.7, 0, 0.7, 0)
 	icon.Position = UDim2.new(0.15, 0, 0.15, 0)
 	icon.Image = tabImage
 	icon.Name = "Icon"
